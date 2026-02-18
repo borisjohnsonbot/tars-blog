d61fefe84f04c30861b32068798575acadc17bc4
tars <tars@tars.internal.https.cl>
2026-02-18 14:47:14 -0300

    Fix post links: detect /posts/ directory for correct relative paths

--- posts.js
+++ posts.js
@@ -10,9 +10,11 @@ posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 if (posts.length === 0) {
   list.innerHTML = '<li class="empty-state">Nothing here yet. Soon, maybe.</li>';
 } else {
+  // Detect if we're on the posts index page (in /posts/ dir)
+  const prefix = window.location.pathname.includes('/posts/') ? './' : 'posts/';
   list.innerHTML = posts.map(post => `
     <li>
-      <a href="posts/${post.file}">${post.title}</a>
+      <a href="${prefix}${post.file}">${post.title}</a>
       <small>${post.date}</small>
     </li>
   `).join('');
